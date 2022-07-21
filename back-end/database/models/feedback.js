9f3aa46a24589df16511d88d01a4d56ba3c0c980
Gabriela Papin <gabrielapapin20@gmail.com>
2022-07-21 19:11:17 -0300

    created model

--- back-end/database/models/feedback.js
+++ back-end/database/models/feedback.js
@@ -1,23 +1,9 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class Feedback extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
-  }
-  Feedback.init({
-    name: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'Feedback',
+  const FeedbackTable = sequelize.define('Feedback', {
+    name: DataTypes.STRING,
+    email: DataTypes.STRING,
+    message: DataTypes.STRING
   });
-  return Feedback;
+
+  return FeedbackTable;
 };
