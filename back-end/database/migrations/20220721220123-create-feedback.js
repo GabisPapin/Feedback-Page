6506c3031049218c9e16724e3b12cfb06c1a1254
Gabriela Papin <gabrielapapin20@gmail.com>
2022-07-21 19:05:01 -0300

    migration created

---
+++ back-end/database/migrations/20220721220123-create-feedback.js
@@ -0,0 +1,33 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Feedbacks', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      email: {
+        type: Sequelize.STRING
+      },
+      message: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, _Sequelize) {
+    await queryInterface.dropTable('Feedbacks');
+  }
+};
