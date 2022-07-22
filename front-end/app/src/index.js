1812141beebcbb169f9a78288994c5475086ad2c
Gabriela Papin <gabrielapapin20@gmail.com>
2022-07-22 15:28:48 -0300

    created form

--- front-end/app/src/index.js
+++ front-end/app/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import { Provider } from 'react-redux';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from 'react-router-dom';
 import store from './services/store/index';
 
@@ -12,10 +12,11 @@ ReactDOM.render(
     <Provider store={ store }>
       <App />
     </Provider>
-  </BrowserRouter>
+  </BrowserRouter>,
+  document.getElementById('root'),
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+serviceWorker.unregister();
