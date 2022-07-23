a031118f8da885f212c1ce40e14d8672b93990e3
Gabriela Papin <gabrielapapin20@gmail.com>
2022-07-22 22:28:03 -0300

    installed cors and link back-end with front-end

--- back-end/api.js
+++ back-end/api.js
@@ -1,11 +1,13 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const feedbackRouter = require('./src/route/feedbackRoute');
 
 const port = process.env.API_PORT;
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.use('/feedback', feedbackRouter);
 
